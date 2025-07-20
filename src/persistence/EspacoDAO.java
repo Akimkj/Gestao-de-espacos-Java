f9690c98517232a712098ac1bad7a6191985ef33
Mika <kakamarques.0202@gmail.com>
2025-07-20 17:30:40 -0300

    add EspacoDAO

--- src/persistence/EspacoDAO.java
+++ src/persistence/EspacoDAO.java
@@ -1,7 +1,28 @@
 package persistence;
-import java.util.*;
+import models.Espaco;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*; 
+import java.util.stream.Stream;
+
 public class EspacoDAO {
     private final String caminhoArquivo = "..\\data\\espacos.txt";
 
-    public List
+    public List<Espaco> listarTodos() {
+        List<Espaco> espacos = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                String[] dados = linha.split(";");
+                int id = Integer.parseInt(dados[0]);
+                String nome = dados[1];
+                String localizacao = dados[2];
+                int capacidade = dados[3];
+                
+            }
+        } 
+        catch (Exception e) {
+        }
+    }
 }
